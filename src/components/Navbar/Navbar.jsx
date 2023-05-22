61825fd704152bbd72265026bbefec9c4dbfc814
Madhushree S <89340929+Madhusiva5@users.noreply.github.com>
2023-05-22 08:32:49 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,8 +35,9 @@ const Navbar = () => {
             <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ul className="app__navbar-smallscreen_links">
               <li><a href="/" onClick={() => setToggleMenu(false)}>Home</a></li>
+                 <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
               <li><a href="/ph" onClick={() => setToggleMenu(false)}><NavLink to="/menu">Menu</NavLink></a></li>
-              <li><a href="/menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+             
               <li><a href="/dec" onClick={() => setToggleMenu(false)}><NavLink to="/dec">Function Decors</NavLink></a></li>
               <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
             </ul>
